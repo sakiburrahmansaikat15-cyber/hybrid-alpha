27e9e43784a9f1a86ef8de893604ae7fb650e91f
sakiburrahmansaikat15-cyber <sakiburrahmansaikat15@gmail.com>
2025-11-09 20:51:52 +0600

    V 1.1

--- resources/js/AppRouter.jsx
+++ resources/js/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Users from "./pages/Dashboard/Users";
 import Layout from "./components/Layout";
+import Roles from "./pages/Dashboard/Roles";
 
 const AppRouter = () => {
     return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Dashboard />} />
                     <Route path="/users/all" element={<Users />} />
+                    <Route path="/roles" element={<Roles />} />
                 </Route>
             </Routes>
         </div>
